61bb99707e3c860ca50fd00a5a7416ba19ed9c94
duchieu <duchieu.ctk41@gmail.com>
2021-09-30 17:52:53 +0700

    friend

--- src/components/friend/Friend.js
+++ src/components/friend/Friend.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
 import './friend.scss'
+import Friends from '../../assets/friends.png'
 
 class Friend extends Component {
   render() {
     return (
-      <div class="friend-container">
-        <div className="title">
-          <div>Người liên hệ</div>
-          <span>Phòng họp</span>
-          <span>Search</span>
-          <span>Option</span>
+      <div class="friend__container">
+        <div className="friend__title">
+          <h4>Người liên hệ</h4>
+          <img src={Friends} alt="hinhanh" />
+          <img src={Friends} alt="hinhanh" />
+          <img src={Friends} alt="hinhanh" />
         </div>
-        <ul className="list-friend">
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
-          <li>Minh Chính</li>
+        <ul className="list__friend">
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
+          <li className="friend__item">
+            <img src={Friends} alt="hinhanh" />
+            <span>Minh Chính</span>
+          </li>
         </ul>
       </div>
     );
